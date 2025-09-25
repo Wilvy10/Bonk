e7187f38b3279c7f8ddc2bedbc82ff58fefe0252
Will Vanlint <william2.vanlint@live.uwe.ac.uk>
2025-09-25 20:24:28 +0100

    small fixes
    added stats for unique posts and fixed collision for drop box

--- sbgj-bonk(unnamed-project)/prefabs/post_stare.gd
+++ sbgj-bonk(unnamed-project)/prefabs/post_stare.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
 var happiness = 5
+var rage = 0
+var stats = [happiness,rage]
+#if you want to add more just add it to the list
 
 func _ready() -> void:
 	get_parent().set_happiness(happiness)
