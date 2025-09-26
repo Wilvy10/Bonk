e0b55141dbcf819b02c90fe18a70874cd04e7692
Will Vanlint <william2.vanlint@live.uwe.ac.uk>
2025-09-26 06:52:31 +0100

    added space for ending
    added state and function for ending

--- sbgj-bonk(unnamed-project)/scripts/generator.gd
+++ sbgj-bonk(unnamed-project)/scripts/generator.gd
@@ -8,30 +8,36 @@ var post_1 = [preload("res://prefabs/stare_post.tscn"),false]
 var user = 1
 var catPostsNum = 2
 var catPosts = [post_0,post_1]
+var times_generated = 0
+
 func _init():
 	SelectPost()
 func setUser(_user):
 	user = _user
 
 func SelectPost():
-	var postsToGenerate = 2
-	var postsLeftToGenerate = 2
-	var postNum = 0
-	while postsLeftToGenerate > 0:
-		if user == 1:
-			#select only cat posts
-			postNum = randi() % catPostsNum - postsToGenerate + postsLeftToGenerate
-		if postNum < 0:
-			postNum = 0
-		postNum = retrieveAvailablePost(catPosts,postNum)
-		#print(postNum)
-		if postNum != null:
-			var postInstance = catPosts[postNum][0].instantiate()
-			add_child(postInstance)
-			postInstance.setStartPosition(locations[postsToGenerate - postsLeftToGenerate])
-			postInstance.set_global_position(locations[postsToGenerate - postsLeftToGenerate])
-			postsLeftToGenerate -= 1
-			catPosts[postNum][1] = true
+	if (times_generated == 10):
+		ending()
+	else:
+		times_generated += 1
+		var postsToGenerate = 2
+		var postsLeftToGenerate = 2
+		var postNum = 0
+		while postsLeftToGenerate > 0:
+			if user == 1:
+				#select only cat posts
+				postNum = randi() % catPostsNum - postsToGenerate + postsLeftToGenerate
+			if postNum < 0:
+				postNum = 0
+			postNum = retrieveAvailablePost(catPosts,postNum)
+			#print(postNum)
+			if postNum != null:
+				var postInstance = catPosts[postNum][0].instantiate()
+				add_child(postInstance)
+				postInstance.setStartPosition(locations[postsToGenerate - postsLeftToGenerate])
+				postInstance.set_global_position(locations[postsToGenerate - postsLeftToGenerate])
+				postsLeftToGenerate -= 1
+				catPosts[postNum][1] = true
 
 
 #list functions
@@ -47,3 +53,6 @@ func retrieveAvailablePost(list:Array,item:int):
 func resetAvailablePosts():
 	for i in catPosts.size():
 		catPosts[i][1] = false
+		
+func ending():
+	pass
